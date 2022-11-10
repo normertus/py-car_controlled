b56b73666daf6a3e4c834f9bd92737a5a6067874
IIIlllIIIlllllllII <115068014+IIIlllIIIlllllllII@users.noreply.github.com>
2022-11-10 12:52:32 +0100

    Brake and forward after right or left

--- main.ts
+++ main.ts
@@ -6,37 +6,22 @@ radio.onReceivedNumber(function (receivedNumber) {
         bitbot.go(BBDirection.Forward, 100)
     }
     if (receivedNumber == 8721) {
-        bitbot.go(BBDirection.Reverse, 100)
-    }
-    if (receivedNumber == 8722) {
         bitbot.stop(BBStopMode.Brake)
     }
     if (receivedNumber == 8724) {
-        bitbot.rotate(BBRobotDirection.Left, 50)
+        bitbot.rotatems(BBRobotDirection.Left, 90, 125)
+        bitbot.go(BBDirection.Forward, 100)
     }
     if (receivedNumber == 8725) {
-        bitbot.rotate(BBRobotDirection.Right, 50)
+        bitbot.rotatems(BBRobotDirection.Right, 90, 125)
+        bitbot.go(BBDirection.Forward, 100)
     }
     if (receivedNumber == 8726) {
-        bitbot.setLedColor(0x8000FF)
+        bitbot.go(BBDirection.Reverse, 100)
     }
 })
-input.onButtonPressed(Button.A, function () {
-    bitbot.rotate(BBRobotDirection.Left, 100)
-    basic.pause(2000)
-})
 radio.setGroup(52)
-basic.showLeds(`
-    # . . . #
-    . # . # .
-    . . # . .
-    . # . # .
-    # . . . #
-    `)
-loops.everyInterval(500, function () {
-    bitbot.BBBias(BBRobotDirection.Left, 25)
-    basic.pause(100)
-})
+bitbot.setLedColor(0x8000FF)
 basic.forever(function () {
     basic.showLeds(`
         . . . . .
@@ -45,7 +30,13 @@ basic.forever(function () {
         . . . . .
         . . # . .
         `)
-    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -53,7 +44,13 @@ basic.forever(function () {
         . # # # .
         . # . # .
         `)
-    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . # . # .
+        . . . . .
+        `)
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -61,7 +58,13 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
-    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     basic.showLeds(`
         . # . # .
         . . . . .
@@ -69,10 +72,12 @@ basic.forever(function () {
         . . . . .
         . . . . .
         `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
     basic.pause(100)
-})
-basic.forever(function () {
-    bitbot.ledBrightness(9999)
-    bitbot.ledRotate()
-    bitbot.ledRainbow()
 })
